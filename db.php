d9aba22b1095b0b207beecad25900e125613c4c9
rroonjha55o2-cell <rroonjha55o2@gmail.com>
2026-08-18 18:59:22 +0500

    Update db.php

--- db.php
+++ db.php
@@ -1,11 +1,14 @@
 <?php
+// Database connection configuration for local server
 $host = "127.0.0.1";
 $user = "root";
 $pass = "root";
 $dbname = "nexus_digital_db";
 
+// Establish connection to MySQL database
 $conn = new mysqli($host, $user, $pass, $dbname);
 
+// Check database connection status
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
